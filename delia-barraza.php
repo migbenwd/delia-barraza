3da98b3015fd261d077e494e80be8d4d6859f97e
migben.com <miguelangel.benitezyanez@gmail.com>
2024-12-10 19:14:28 -0400

    func: ya comprueba carpeta "servicio" en el dominio

--- delia-barraza.php
+++ delia-barraza.php
@@ -35,17 +35,22 @@ function wpgrp_menu_cb(){
 }
 
 
-/****************************************************************/
 
 
-function get_all_products_tijuana(){
+
+
+function get_servicios_por_ciudad($cityId){
+	
+	if (isset($_POST['cityId'])) {
+        $cityId = sanitize_text_field($_POST['cityId']); // Sanitizamos el valor para prevenir inyecciones	
+	}
 
 	$result = [];
 	$data = [];
 	$index = 0;
 	$contar = 0;
 	
-		// ------------ Consulta TOKEN ------------------------------------// 
+	// ------------ Consulta TOKEN ------------------------------------// 
 	
 	$usuario = "DELIABARRAZA";
 	$password = "RGVsaWFiYXJyYXphMTIzIQ==";
@@ -74,7 +79,10 @@ function get_all_products_tijuana(){
 
 	$url_servicio = "https://deliabarraza.sass.com.mx/ws1/?token=" . $token_servicio;
 
-	$data_param = json_encode(['request' => 'WCE', 'client' => 'deliabarraza', 'company' => '0002' ]);
+	$data_param = json_encode(['request' => 'WCE', 
+							   'client' => 'deliabarraza', 
+							   'company' => $cityId 
+							  ]);
 
 	$ch = curl_init();
 
@@ -126,13 +134,6 @@ function get_all_products_tijuana(){
 		}
 
 		$data['result'] = $result;
-
-		// Función de comparación para ordenar por 'studie'
-		/*
-		function compararEstudios($a, $b) {
-		return strcmp($a['studie'], $b['studie']);
-		}
-		*/
 		
 		// ------ * FUNCION PARA ORDENAR POR CODIGO DE SERVICIO DE FORMA ASCENDENTE
 		
@@ -154,6 +155,7 @@ function get_all_products_tijuana(){
 		// Ordenar el arreglo 'result' usando la función de comparación
 		usort($data['result'], 'compararEstudios');
 		$data_api = json_encode($data);
+		
 		echo $data_api;
 		
 		die();
@@ -163,22 +165,28 @@ function get_all_products_tijuana(){
 	curl_close($ch);
 }
 
-add_action('wp_ajax_get_all_products_tijuana', 'get_all_products_tijuana');  // Para usuarios logueados
-add_action('wp_ajax_nopriv_get_all_products_tijuana', 'get_all_products_tijuana');  // Para usuarios no logueados
+add_action('wp_ajax_get_servicios_por_ciudad', 'get_servicios_por_ciudad');  // Para usuarios logueados
+add_action('wp_ajax_nopriv_get_servicios_por_ciudad', 'get_servicios_por_ciudad');  // Para usuarios no logueados
+
+
 
 
 function enqueue_ajax_script() {
     // Obtener el directorio del plugin
     $plugin_url = plugin_dir_url(__FILE__);
-
+	
     // Registrar el script
-    wp_register_script(
+    
+	wp_register_script(
         'ajax-products-script',  // Handle
         $plugin_url . 'js/script.js',  // Ubicación del archivo
         ['jquery'],  // Dependencias
         '1.0',  // Versión
         true  // Cargar en el footer
     );
+	
+	
+	// wp_enqueue_script( 'mi-script', plugins_url( 'js/script.js', __FILE__ ), array('jquery') );
 
     // Localizar el script para enviar el URL de AJAX
     wp_localize_script(
@@ -349,7 +357,7 @@ function get_all_products(){
 				$post_args = array(
 					'ID'           => $post_id,
 					// Aquí agregarías los campos a actualizar, por ejemplo:
-					'post_title'   => $estudio['studie'],
+					'post_title'   => 'Update ' . $estudio['studie'],
 					'post_content' => $estudio['indications'],
 				);
 				
@@ -367,7 +375,7 @@ function get_all_products(){
 	
 				$nueva_publicacion = array(
 					
-					'post_title'   => $estudio['studie'],
+					'post_title'   => 'Insert ' . $estudio['studie'],
 					'post_content' => $estudio['indications'],
 					'post_status'   => 'publish',
 					'post_type'     => 'servicios',
@@ -404,198 +412,105 @@ add_action('admin_post_nopriv_get_all_products','get_all_products');
 
 function mostrar_cod_servicio() {
 	$cod_servicio = get_post_meta( get_the_ID(), 'cod_servicio_deliabarraza', true );
-	return "<p class='clase-servicio-da-card' post_id_servicio_medico=". get_the_ID() ." cod_servicio_medico=". $cod_servicio ." style='font-size: 25px; font-weight: 700;'>0.00</p>";
+	return "<p class='clase-servicio-da-card jet-listing-dynamic-field__content' post_id_servicio_medico=". get_the_ID() ." cod_servicio_medico=". $cod_servicio ." style='font-weight: 800; font-size:     23px;'>0.00</p>";
 }
 add_shortcode( 'cod_servicio_medico_deliabarraza', 'mostrar_cod_servicio' );
 
 
+// ----------------------------------- MODAL CIUDADES ------------------------------------------------------------------------//
 
 
+function mostrar_modal_de_ciudades() {
 
-
-
-
-
-/*
-function get_all_products(){
-
-	$result = [];
-	$data = [];
-	$index = 0;
-	$contar = 0;
-	$updates = 0;
-	
-	$usuario = "DELIABARRAZA";
-	$password = "RGVsaWFiYXJyYXphMTIzIQ==";
-	$url_token = "https://deliabarraza.sass.com.mx/ws0";
-	
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url_token); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_USERPWD, "$usuario:$password");
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);	
-	
-	$token_1 = curl_exec($ch); 
-	$token_1 = json_decode($token_1); 
-	
-	curl_close($ch);
-	
-	
-	$usuario = "Interfacepacs";
-	$password = "Abc123456789$";
-	$token_servicio = $token_1->respuesta;
-
-	$url_servicio = "https://deliabarraza.sass.com.mx/ws1/?token=" . $token_servicio;
-
-	$data_param = json_encode(['request' => 'WCE', 'client' => 'deliabarraza', 'company' => '9999' ]);
-
-	$ch = curl_init();
-
-	curl_setopt($ch, CURLOPT_URL, $url_servicio);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_USERPWD, "$usuario:$password");
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_param);
-
-	$servicios = curl_exec($ch);
-
-	if (curl_errno($ch)) {
-	  echo 'Error: ' . curl_error($ch);
-	} else {
-	  
-		  
-	// Decodificar el JSON en un array asociativo de PHP
-	$datos = json_decode($servicios, true);
-
-	$estudios = $datos['data']['studies'];
-
-	foreach ($estudios as $estudio) {
-
-		// Obtener la fecha y hora actual en un formato legible
-		$fecha_hora = date('Y-m-d H:i:s');
-
-		
-			if($contar == 7 ){
-				break;
+echo '<div id="modal-ciudades" class="modal">
+				<div class="modal-content">
+					<h2>Selecciona tu ciudad</h2>
+					<p>Conoce los precios y servicios que existen en donde vives</p>
+					<br>
+					<div class="container"> 
+						<div class="row modal-ciudades"> 
+							<div class="col-md-2" data-ciudad-id="0002" style="cursor: pointer;">Tijuana</div>
+							<div class="col-md-2" data-ciudad-id="001" style="cursor: pointer;">Sinaloa</div>
+							<div class="col-md-2" data-ciudad-id="3" style="cursor: pointer;">Otra Ciudad</div>
+							<div class="col-md-2" data-ciudad-id="4" style="cursor: pointer;">Otra Ciudad</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<style>
+			.modal-ciudades div:hover {
+			  background-color: fuchsia;
 			}
 			
-			$result[$index]['code'] = $estudio['code'];
-			$result[$index]['studie'] = $estudio['studie'];
-			$result[$index]['price'] = $estudio['price'];
-			$result[$index]['indications'] = $estudio['indications'][0]['indication'];
-					
-
-			$updates++;
-			$index++;
-			
-
-
-
-			// Crear o actualizar el post
-			$args = array(
-				'meta_query' => array(
-					array(
-						'key'     => 'cod_servicio_deliabarraza',
-						'value'   => $estudio['code'],
-						'compare' => '='
-					)
-				),
-				'post_type' => 'servicios',
-				'posts_per_page' => 1
-			);
-
-			$posts = get_posts($args);
-
-			if ($posts) {
-				
-				// Si se encontró el post, actualizarlo
-				$post_id = $posts[0]->ID;
-				$post_args = array(
-					'ID'           => $post_id,
-					// Aquí agregarías los campos a actualizar, por ejemplo:
-					'post_title'   => 'Update título -' . $estudio['studie']. ' - ' . $fecha_hora ,
-					'post_content' => 'Update contenido ' . $estudio['indications'][0]['indication'] . ' - ' . $fecha_hora,
-				);
-				
-				wp_update_post($post_args);
-				
-        		update_post_meta($post_id, 'precio_oferta', 666);
-
-			
-			} else {
-				// Si no se encontró, crear un nuevo post
-				// Crear SERVICIOS (Post) ----------------------------------------------------
-	
-				// Obtener la fecha y hora actual en un formato legible
-				$fecha_hora = date('Y-m-d H:i:s');
-	
-				$nueva_publicacion = array(
-					
-					'post_title'   => 'Insert Migben SERVICIO DELIA BARRAZA - ' . $estudio['studie'] . '- FECHA - '. $fecha_hora,
-					'post_content' => 'Insert Contenido Indicaciones - ' . $estudio['indications'][0]['indication'],
-					'post_status'   => 'publish',
-					'post_type'     => 'servicios',
-				);
-	
-				$post_id = wp_insert_post($nueva_publicacion);
-			   
-				update_post_meta($post_id, 'cod_servicio_deliabarraza', $estudio['code']);
-        		update_post_meta($post_id, 'precio_oferta', 332);
-					
-				// -------------------------------------------------------------------------------
+			.modal {
+			display: block;
+			position: fixed;
+			z-index: 9999;
+			left: 0;
+			top: 0;
+			width: 100%;
+			height: 100%;
+			overflow: auto;
+			background-color: rgba(0,0,0,0.4);
 			}
 
-			$contar++;
+			.modal-content {
+			background-color: #fefefe;
+			margin: 15% auto;
+			padding: 2%;
+			border: 1px solid #888;
+			width: 50%;
+			text-align: center;
+			}
 
-		}
+			.modal-ciudades div {
+			
+			background: white;
+			border: 2px solid black;
+			padding: 2%;
+			margin: 10px 2px 13px 23px;
+			width: 45%;
+			border-radius: 11px;
 
-		
-		$data['result'] = $result;
-		
-		
+			}
+			.modal.show {
+				animation: fadeInFromBottom 0.5s ease-in-out;
+			}
 
-		// Función de comparación para ordenar por 'studie'
+			@keyframes fadeInFromBottom {
+				0% {
+					opacity: 0;
+					transform: translateY(100%);
+				}
+				100% {
+					opacity: 1;
+					transform: translateY(0);
+				}
+			}
 		
-		// function compararEstudios($a, $b) {
-		// return strcmp($a['studie'], $b['studie']);
-		// }
-		
-		
-		
-		function compararEstudios($a, $b) {
-		$codigoA = intval($a['code']);
-		$codigoB = intval($b['code']);
+			.modal.ocultar {
+				animation: fadeOutFromTop 4s ease-in-out;
+			}
 
-		// Comparar los códigos numéricos
-		if ($codigoA === $codigoB) {
-			return 0; // Si son iguales, no importa el orden
-		} else {
-			return ($codigoA < $codigoB) ? -1 : 1; // Orden ascendente
-		}
-		}
-
-		// Ordenar el arreglo 'result' usando la función de comparación
-		usort($data['result'], 'compararEstudios');
-		
-		$data_api = json_encode($data);
-		
+			@keyframes fadeOutFromTop {
+				0% {
+					opacity: 1;
+					transform: translateY(0);
+				}
+				100% {
+					opacity: 0;
+					transform: translateY(-100%);
+				}
+			}
+		</style>';
 	
-		// print $data_api;
-		echo $data_api;
-		
-		// return $data_api;
-		
-
-	}
-
-	curl_close($ch);
+		die();	
 }
+add_action('wp_ajax_mostrar_modal_de_ciudades', 'mostrar_modal_de_ciudades');
+add_action('wp_ajax_nopriv_mostrar_modal_de_ciudades', 'mostrar_modal_de_ciudades');
 
-add_action('admin_post_get_all_products','get_all_products');
-add_action('admin_post_nopriv_get_all_products','get_all_products');
-
-*/
+function estilos_modal_ciudades() {
+	wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css', array(), '5.3.0');
+ 	wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.0', true);	
+}
+add_action('wp_enqueue_scripts', 'estilos_modal_ciudades');
--- js/script.js
+++ js/script.js
@@ -1,43 +1,142 @@
 (function($){
 	
 	$(document).ready(function(){
+		
+		
+		// Llamamos a la función para verificar
+		existeCiudadSeleccionada();
+		comprobarCarpeta();
+		
+		function existeCiudadSeleccionada() {
+		  // Obtenemos el valor del localStorage con la clave "city_selected_id"
+		  const ciudadSeleccionada = localStorage.getItem('city_selected_id');
+	
+		  // Verificamos si el valor obtenido es null (no existe) o una cadena vacía
+		  if (ciudadSeleccionada === null || ciudadSeleccionada === '') {
+			console.log('No existe una ciudad seleccionada en el localStorage.');
+			
+			 // Mostramos el MODAL - POP UP  
+			 MostrarModal();
+			
+			return false;
+		  } else {
+			console.log('Existe una ciudad seleccionada:', ciudadSeleccionada);
+			return true;
+		  }
+		}
+		
+		function comprobarCarpeta(){
+	
+			const urlActual = window.location.href;
+			const contieneServicio = urlActual.includes('servicio');
+	
+			if (contieneServicio) {
+				console.log("El dominio contiene la carpeta 'servicio'");
+				// Si existe city_selected_id en LOCAL ESTORAGE...pues... reestablecer precio
+				reestablecerPrecios();
+			} else {
+				console.log("El dominio NO contiene la carpeta 'servicio'");
+			}		
+		}
+		
+	
+		// ------------------------------------------------------------------------------------------------------------------//
+		
+		
+		function MostrarModal(){
+		$.ajax({
+			url: ajaxParams.ajaxUrl,  // El URL de WordPress, usualmente 'wp-admin/admin-ajax.php'
+			type: 'POST',
+			data: {
+				action: 'mostrar_modal_de_ciudades'
+			},
+			success: function(response) {
+			$('body').append(response);	
+			showModal();
+			},
+			error: function(error) {
+				console.log('Dió Error: ');
+				console.log('Error: ', error);
+			}
+		});
+		}
+		
+		function showModal(){
+			console.log('entro en showModal');
+			const modal = document.getElementById('modal-ciudades');
+			modal.classList.add('show');
+			
+	
+			// Seleccionamos el div con el ID "modal-ciudades"
+			const modalCiudades = document.getElementById('modal-ciudades');
+	
+			// Agregamos un event listener para detectar clics dentro del div
+			modalCiudades.addEventListener('click', (event) => {
+			  // Verificamos si el elemento en el que se hizo clic tiene el atributo data-ciudad-id
+			  if (event.target.hasAttribute('data-ciudad-id')) {
+				// Obtenemos el valor del atributo y lo mostramos en consola (puedes reemplazar esto con cualquier otra acción)
+				const ciudadId = event.target.dataset.ciudadId;
+				console.log('El ID de la ciudad es:', ciudadId);
+				  
+				localStorage.setItem('city_selected_id', ciudadId);
+	
+				modal.classList.remove('show'); // Ocultar el modal
+				modal.style.transition = 'transform 0.5s ease-in-out';
+				modal.style.transform = 'translateY(-100%)';
+				  
+				setTimeout(() => {
+					  modal.style.display = 'none';
+				  }, 800); // 5000 milisegundos = 5 segundos
+				var CiudadSelectId =  localStorage.getItem('city_selected_id');
+	
+				get_servicios_por_ciudad(CiudadSelectId);			  
+	
+			  }
+			});
+			
+			
+		}
 	
 		
 		let animacionActiva = false; // Bandera para controlar la animación	
 		
-		getServiciosTijuana();
+		// getServiciosTijuana();
 		
-		function getServiciosTijuana(){
+		function get_servicios_por_ciudad($cityId){
 		
 		// Marca el inicio de la consulta
 		const inicio = new Date();
-		console.log('Inicio de la consulta - getServiciosTijuana', inicio.toLocaleString());
+		console.log('Inicio de la consulta - getServicios', inicio.toLocaleString());
 		
-		activarAnimacion();	
+		// activarAnimacion();	
 		
 		$.ajax({
 			url: ajaxParams.ajaxUrl,  // El URL de WordPress, usualmente 'wp-admin/admin-ajax.php'
 			type: 'POST',
 			data: {
-				action: 'get_all_products_tijuana'  // El nombre de la acción que registramos
+				action: 'get_servicios_por_ciudad',
+				cityId: $cityId
 			},
 			success: function(response) {
-	
+			
+				
 			let dataServicio = JSON.parse(response);
+				
+			console.log('respuesta desde get_servicios_por_ciudad en dataServicio', dataServicio);	
 	
 			// Guardar el array en local storage
-			localStorage.setItem('serviciosTijuana', JSON.stringify(dataServicio.result));
+			localStorage.setItem('servicios_city', JSON.stringify(dataServicio.result));
 			console.log('Datos guardados en local storage');
 	
 			// Recuperar y parsear los datos del local storage
 					
-			const datosGuardados = JSON.parse(localStorage.getItem('serviciosTijuana'));					
+			const datosGuardados = JSON.parse(localStorage.getItem('servicios_city'));					
 			const primerEstudio = datosGuardados[0].studie;
 			// console.log('Primer campo de studie:', primerEstudio);			
 			
-			reestablecerPrecios();
+			// reestablecerPrecios();
 	
-			desactivarAnimacion();	
+			// desactivarAnimacion();	
 				
 			const fin = new Date();
 			console.log('Fin de la consulta:', fin.toLocaleString());
@@ -62,7 +161,7 @@
 		  // console.log('Entra en encontrarPrecioPorCodigo');
 	
 		  // Obtener el array de servicios desde el localStorage
-		  const servicios = JSON.parse(localStorage.getItem('serviciosTijuana'));
+		  const servicios = JSON.parse(localStorage.getItem('servicios_city'));
 	
 		  // Buscar el servicio con el código dado
 		  const servicioEncontrado = servicios.find(servicio => servicio.code === codigo);
@@ -73,7 +172,7 @@
 	
 		
 		
-			function reestablecerPrecios() {
+		function reestablecerPrecios() {
 		// Selecciona todos los elementos con la clase "clase-servicio-da-card"
 		const elementosServicio = document.querySelectorAll('.clase-servicio-da-card');
 	
@@ -95,91 +194,84 @@
 		}		
 		
 		
-		// --------- ANIMACION 
+			// --------- ANIMACION 
+	
+	
+		function AnimacionBusqueda() {
+		if (animacionActiva) {
+		console.log('entró en ANIMACION');	
+	
+		// Obtén el div contenedor por su atributo data-id
+		const contenedor = document.querySelector('div[data-id="ccae5cb"]');
+	
+		// Crea el div interno
+		const divCargando = document.createElement('div');
+		divCargando.style.backgroundColor = '#03B0AB';
+		divCargando.style.position = 'absolute';
+		divCargando.style.top = 0;
+		divCargando.style.left = 0;
+		divCargando.style.width = '100%';
+		divCargando.style.height = '100%';
+		divCargando.style.display = 'flex';
+		divCargando.style.alignItems = 'center';
+		divCargando.style.justifyContent = 'center';
+		divCargando.id = "migben-animator";
+		divCargando.textContent = 'Cargando precios...';	
+		divCargando.style.fontSize = '50px';
+		divCargando.style.color = 'white';
+	
+	
+		// Crea una imagen para el GIF
+		/*
+		const imgLoader = document.createElement('img');
+		imgLoader.src = 'https://i.gifer.com/ZZ5H.gif'; // Reemplaza con la ruta de tu GIF
+		divCargando.appendChild(imgLoader);
+		*/
+	
+		// Agrega el div interno al contenedor
+		contenedor.appendChild(divCargando);
+	
+		// Función para cambiar la opacidad (disminuye ligeramente)
+		  divCargando.style.opacity = 0.7; // Opacidad inicial (puedes ajustar)
+	
+		// Función para cambiar la opacidad
+		/*
+		function cambiarOpacidad() {
+		  divCargando.style.opacity = divCargando.style.opacity === '0.5' ? '1' : '0.5';
+		}
+	
+		// Ejecuta la función cada segundo
+		setInterval(cambiarOpacidad, 500);	
+		*/
+	
+	
+		}
+		}	
+	
+		function activarAnimacion() {
+		  animacionActiva = true;
+		  AnimacionBusqueda();
+		}
+	
+		function desactivarAnimacion() {
+		  animacionActiva = false;
+		  // Aquí puedes agregar código para eliminar el div de carga si es necesario
+		  // Por ejemplo:
+		  const divCargando = document.querySelector('div[id="migben-animator"]');
+		  if (divCargando) {
+			divCargando.remove();
+			// divCargando.hide();
+		  }
+		}	
 		
 		
-	function AnimacionBusqueda() {
-	if (animacionActiva) {
-	console.log('entró en ANIMACION');	
-	
-	// Obtén el div contenedor por su atributo data-id
-	const contenedor = document.querySelector('div[data-id="ccae5cb"]');
-	
-	// Crea el div interno
-	const divCargando = document.createElement('div');
-	divCargando.style.backgroundColor = '#03B0AB';
-	divCargando.style.position = 'absolute';
-	divCargando.style.top = 0;
-	divCargando.style.left = 0;
-	divCargando.style.width = '100%';
-	divCargando.style.height = '100%';
-	divCargando.style.display = 'flex';
-	divCargando.style.alignItems = 'center';
-	divCargando.style.justifyContent = 'center';
-	divCargando.id = "migben-animator";
-	divCargando.textContent = 'Cargando precios...';	
-	divCargando.style.fontSize = '50px';
-	divCargando.style.color = 'white';
-		
-	
-	// Crea una imagen para el GIF
-	/*
-	const imgLoader = document.createElement('img');
-	imgLoader.src = 'https://i.gifer.com/ZZ5H.gif'; // Reemplaza con la ruta de tu GIF
-	divCargando.appendChild(imgLoader);
-	*/
-	
-	// Agrega el div interno al contenedor
-	contenedor.appendChild(divCargando);
-	
-	// Función para cambiar la opacidad (disminuye ligeramente)
-	  divCargando.style.opacity = 0.7; // Opacidad inicial (puedes ajustar)
-		
-	// Función para cambiar la opacidad
-	/*
-	function cambiarOpacidad() {
-	  divCargando.style.opacity = divCargando.style.opacity === '0.5' ? '1' : '0.5';
-	}
-	
-	// Ejecuta la función cada segundo
-	setInterval(cambiarOpacidad, 500);	
-	*/
-		
-		
-	}
-	}	
-	
-	function activarAnimacion() {
-	  animacionActiva = true;
-	  AnimacionBusqueda();
-	}
-	
-	function desactivarAnimacion() {
-	  animacionActiva = false;
-	  // Aquí puedes agregar código para eliminar el div de carga si es necesario
-	  // Por ejemplo:
-	  const divCargando = document.querySelector('div[id="migben-animator"]');
-	  if (divCargando) {
-		divCargando.remove();
-		// divCargando.hide();
-	  }
-	}	
-		
-		
-	// ----- EVENTO QUE SE DISPARA...CUANDO HACEN CLICK EN ALGUN FILTRO
+		// ----- EVENTO QUE SE DISPARA...CUANDO HACEN CLICK EN ALGUN FILTRO
 	
 			
-	$(document ).on( 'jet-filter-content-rendered', function() {
-	
-		console.log('click en filtros jet smart');
-		reestablecerPrecios();
-	
-	
-	});
-	
-		
-		
-		
+		$(document ).on( 'jet-filter-content-rendered', function() {
+			console.log('click en filtros jet smart');
+			reestablecerPrecios();
+		});
 		
 			
 			
